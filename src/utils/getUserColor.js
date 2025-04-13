791c3e2380c6086b1130828f1f9486e00e72b27a
ibrahim Çelik <ibrahimcelik@MacBookAir.hgw.local>
2025-04-13 14:14:38 +0300

    edit code

--- src/utils/getUserColor.js
+++ src/utils/getUserColor.js
@@ -7,7 +7,7 @@ export default function getUserColor(user) {
 function stringToHash(str) {
   let hash = 0;
   for (let i = 0; i < str.length; i++) {
-    const char = str.charCodeAt(i);.
+    const char = str.charCodeAt(i);
     hash = (hash << 5) - hash + char;
     hash |= 0; // 32-bit integer'e çevir
   }
